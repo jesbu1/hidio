b5fbfa0ff182b17ab56d1f1750e4855b1a593b8b
Jesse Zhang <jessezhang5@gmail.com>
2021-01-22 13:44:19 -0800

    alf update

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import os
 setup(
     name='hidio',
     version='1.0',
-    install_requires=[
-        'pybind11==2.5.0',
-        #'alf@git+https://github.com/horizonrobotics/alf.git@1146c4c78aef06a958c1f0c1d83be6645b11cc31'
-    ],
+    #install_requires=[
+    #    'pybind11==2.5.0',
+    #    #'alf@git+https://github.com/jesbu1/alf.git',
+    #],
 )
 #os.system("pip install -e ./hidio/cnest")
